47b5c1bf4d9582c7ec8f17910444446a3e5c0c8a
Daniil Shipitsin <danshipitsin@gmaail.com>
2024-12-15 01:05:05 +0200

    2. Changes to the dataset.py file dataset.py has become more readable and adaptive, all its work has been converted into one CreateDataset class

--- 3_python_files/dataset.py
+++ 3_python_files/dataset.py
@@ -4,82 +4,91 @@ import os
 import pandas as pd
 import numpy as np
 
-img = cv2.imread(
-    "../1_data/1_1_raw_data/rooms_images/airport_inside/airport_inside_0001.jpg"
+
+#! Изменить путь для создания файла parquet
+
+
+class CreateDataset:
+    def __init__(self, dataset_path):
+        self.dataset_path = dataset_path
+
+    def create_a_list_of_classes(self):
+        folders_list = [
+            folder
+            for folder in os.listdir(self.dataset_path)
+            if os.path.isdir(os.path.join(self.dataset_path, folder))
+        ]
+
+        self.folders_dict = {}
+        for class_number, class_name in enumerate(folders_list):
+            self.folders_dict[class_name] = class_number
+
+        print(f"Найдено {len(self.folders_dict)} классов")
+        print(f"Cписок всех классов: {self.folders_dict}")
+
+    def create_image_list(self):
+        self.images_list = []  # Список для хранения путей и классов изображений
+
+        for current_path, subdirs, files in os.walk(self.dataset_path):
+            for file in files:
+                if file.lower().endswith(
+                    (".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff")
+                ):
+                    image_path = os.path.join(current_path, file)
+                    class_label = self.folders_dict[
+                        os.path.basename(current_path)
+                    ]  # Класс текущего изображения
+                    self.images_list.append(
+                        (image_path, class_label)
+                    )  # Добавляем путь и класс в виде кортежа
+
+        # Вывод количества найденных изображений
+        print(f"Найдено {len(self.images_list)} изображений")
+
+    def create_dataset(self):
+        self.dataset_images = []
+        self.dataset_classes = []
+
+        for image, num_class in self.images_list:
+            image = cv2.imread(filename=image)
+            if image is not None:
+                image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
+                image = cv2.resize(
+                    src=image, dsize=(256, 256), interpolation=cv2.INTER_CUBIC
+                )
+                self.dataset_images.append(image)
+                self.dataset_classes.append(num_class)
+
+        print(
+            f"Размер каждой картинки: {len(self.dataset_images[1000])} x {len(self.dataset_images[1000][0])}"
+        )
+
+        self.dataset_images = np.array(self.dataset_images)
+        self.dataset_classes = np.array(self.dataset_classes)
+
+        self.dataset = pd.DataFrame(self.dataset_images.reshape(15590, -1))
+        self.dataset["class"] = self.dataset_classes
+
+        print(f"Размер датасета: {self.dataset.shape}")
+        print("Конвертация датасета в файл parquet")
+
+        self.dataset.to_parquet("dataset.parquet")
+
+        print("Завершено успешно!")
+
+    def __call__(self):
+        print("    Выполнение всех команд для создания датасета:")
+        print("    Создание списка классов изображений:")
+        self.create_a_list_of_classes()
+        print("    Создание списка всех изображений в виде numpy.array:")
+        self.create_image_list()
+        print("    Создание датасета")
+        self.create_dataset()
+
+
+cls = CreateDataset(
+    dataset_path=r"C:\All_Enough\Pseudocode\Projects\Pet_project_clf_rooms_mit\1_data\1_1_raw_data\rooms_images"
 )
-print(img)
-img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+cls()
 
-plt.imshow(img, cmap="gray")
-
-
-img = cv2.resize(src=img, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
-plt.imshow(img, cmap="gray")
-
-
-# Укажите путь к директории
-root_dir = r"C:\All_Enough\Pseudocode\Projects\Pet_project_clf_rooms_mit\1_data\1_1_raw_data\rooms_images"
-
-# Подсчитываем количество папок
-folder_count = sum(len(subdirs) for _, subdirs, _ in os.walk(root_dir))
-
-print(f"Количество вложенных папок: {folder_count}")
-
-folders_list = [
-    folder
-    for folder in os.listdir(root_dir)
-    if os.path.isdir(os.path.join(root_dir, folder))
-]
-folders_dict = {}
-for class_number, class_name in enumerate(folders_list):
-    folders_dict[class_name] = class_number
-
-
-print(folders_dict)
-# Укажите путь к директории
-
-root_dir = r"C:\All_Enough\Pseudocode\Projects\Pet_project_clf_rooms_mit\1_data\1_1_raw_data\rooms_images"
-
-# Создаем список для всех путей к изображениям
-image_paths = []
-
-# Проходим по всем подпапкам
-for current_path, subdirs, files in os.walk(root_dir):
-    for file in files:
-        # Проверяем, является ли файл изображением
-        if file.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff")):
-            # Добавляем полный путь к изображению
-            image_paths.append(os.path.join(current_path, file))
-
-# Вывод списка изображений
-print(f"Найдено {len(image_paths)} изображений")
-for path in image_paths[600:620]:  # Выводим первые 5 изображений для примера
-    print(path)
-
-print(image_paths[1856])
-
-cnt = 0
-images = []
-for img in image_paths:
-    img = cv2.imread(filename=img)
-    cnt += 1
-    print(cnt)
-
-    if img is None:
-        continue
-    else:
-        img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(src=img, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
-        images.append(img)
-
-
-print("Картины хранятся в images")
-print(f"Размер каждый картинки: {len(images[1000])} x {len(images[1000][0])}")
-
-columns_name = [f"column_{i}" for i in range(1, 257)]
-
-images = np.array(images)
-df = pd.DataFrame(images.reshape(15590, -1))
-print(df.shape)  # (15590, 65536)
-
-df.to_parquet("lol.parquet")
+data = pd.read_parquet("dataset.parquet")
---
+++ 3_python_files/feature_engineering.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+data = pd.read_parquet('../1_data/1_2_intermediate data/lol.parquet')
